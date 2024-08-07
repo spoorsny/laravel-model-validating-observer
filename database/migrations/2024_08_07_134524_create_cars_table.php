6d60e7cb29d55b16fe96f84b6aa9ea1fdaf8c8de
Geoffrey Bernardo van Wyk <geoffrey@vanwyk.biz>
2024-08-07 19:01:47 +0200

    docs: add copyright notices to top of files
    Relates to #2

--- database/migrations/2024_08_07_134524_create_cars_table.php
+++ database/migrations/2024_08_07_134524_create_cars_table.php
@@ -1,5 +1,20 @@
 <?php
 
+// This file is part of package spoorsny/laravel-model-validating-observer.
+//
+// Package spoorsny/laravel-model-validating-observer is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Package spoorsny/laravel-model-validating-observer is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// package spoorsny/laravel-model-validating-observer. If not, see <https://www.gnu.org/licenses/>.
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
